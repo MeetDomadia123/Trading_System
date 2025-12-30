0669fab9cb03458400dc26e4a42aabe122bd0808
Meet <meetdomadia789@gmail.com>
2025-12-30 21:44:14 +0530

    added alt tag in image

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -38,7 +38,7 @@ const UserDropdown = () => {
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500 ">
                     <Avatar className="h-8 w-8">
-                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarImage src="https://github.com/shadcn.png" alt={`${user.name}'s avatar`}/>
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">{user.name[0]}</AvatarFallback>
                     </Avatar>
                     <div className="hidden md:flex flex-col items-start">
